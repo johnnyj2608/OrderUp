62652ea5e0ddb6301605c7b10287d8a5b3a25bef
Johnny Jiang <johnnyj2608@gmail.com>
2024-07-18 17:31:25 -0400

    Get sheet names

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require("express");
-
 const {google} = require("googleapis");
 
 const app = express();
 
 app.set("view engine", "ejs");
-
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.render("index");
+// app.get("/", (req, res) => {
+//     res.render("index");
+// });
+
+app.get("/", async (req, res) => {
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
+
+    const client = await auth.getClient();
+    const googleSheets = google.sheets({version: "v4", auth: client});
+    const spreadsheetId = "1eNlUPP-Cw50W5PIjTy6HchqL6Yo12KFkAqydLvQsI8M";
+
+    const spreadsheet = await googleSheets.spreadsheets.get({
+        auth,
+        spreadsheetId,
+    });
+
+    const sheetNames = spreadsheet.data.sheets.map(sheet => sheet.properties.title);
+
+    res.render("index", { sheetNames });
 });
 
 app.post("/", async (req, res) => {
@@ -21,28 +39,16 @@ app.post("/", async (req, res) => {
         scopes: "https://www.googleapis.com/auth/spreadsheets",
     });
 
-    // Create client instance for auth
     const client = await auth.getClient();
-
-    // Instance of Google Sheets API
     const googleSheets = google.sheets({version: "v4", auth: client });
-
     const spreadsheetId = "1eNlUPP-Cw50W5PIjTy6HchqL6Yo12KFkAqydLvQsI8M";
 
-    // Get metadata about spreadsheet
-    const metaData = await googleSheets.spreadsheets.get({
-        auth,
-        spreadsheetId,
-    });
-
-    // Read rows from spreadsheet
     const getRows = await googleSheets.spreadsheets.values.get({
         auth,
         spreadsheetId,
         range: "Menu!A7:E12",
     });
 
-    // Write row(s) to spreadsheet
     await googleSheets.spreadsheets.values.append({
         auth,
         spreadsheetId,
