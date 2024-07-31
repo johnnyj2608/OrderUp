0ff2e3ac745ee74c4845146a3fd9d45bfab5c2af
Johnny Jiang <johnnyj2608@gmail.com>
2024-07-31 12:21:20 -0400

    Numpad template

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,28 @@
 function handleButtonClick(sheetName) {
-    document.querySelectorAll('button').forEach(button => {
+    document.querySelectorAll('button.panel').forEach(button => {
         button.classList.remove('selected');
     });
 
     document.getElementById(`btn-${sheetName}`).classList.add('selected');
+}
+
+let display = document.querySelector("#display");
+let buttons = document.querySelectorAll('.btn');
+let clear = document.querySelector("#clearBtn");
+let back = document.querySelector("#backBtn");
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', () => {
+        display.innerHTML += buttons[i].innerHTML;
+
+    });
+
+    clear.addEventListener('click', () => {
+        display.innerHTML = "";
+    })
+
+    back.addEventListener('click', () => {
+        display.innerHTML = display.innerHTML.slice(0, -1);
+    })
+    
 }
