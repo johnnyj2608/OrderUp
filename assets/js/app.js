8227fb1530a74d1b3be382c8c03929651206f90a
Johnny Jiang <johnnyj2608@gmail.com>
2024-07-31 09:44:31 -0400

    Buttons for Insurance

---
+++ assets/js/app.js
@@ -0,0 +1,3 @@
+function handleButtonClick(sheetName) {
+    console.log(sheetName);
+}
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./"));
 
 app.get("/", async (req, res) => {
     const auth = new google.auth.GoogleAuth({
