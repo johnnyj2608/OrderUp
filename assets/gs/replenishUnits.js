ee8a6157a42d6f6c2aeb631d1720ed4bedd32d1d
Johnny <johnnyj2608@gmail.com>
2024-08-08 14:14:01 -0400

    Replenish units weekly

---
+++ assets/gs/replenishUnits.js
@@ -0,0 +1,20 @@
+function replenishUnits() {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet();
+  const sheets = sheet.getSheets();
+  const excludeSheets = ["Breakfast", "Lunch", "Menu", "QR"];
+
+  sheets.forEach(sheet => {
+    const sheetName = sheet.getName();
+    if (!excludeSheets.includes(sheetName)) {
+      const range = sheet.getDataRange();
+      const rows = range.getNumRows();
+
+      for (let i = 2; i<= rows; i++) {
+        const schedule = sheet.getRange(i, 4).getValue();
+        const scheduleParsed = schedule.replace(/\./g, '');
+        const units = scheduleParsed.length;
+        sheet.getRange(i, 5).setValue(units)
+      }
+    }
+  })
+}
