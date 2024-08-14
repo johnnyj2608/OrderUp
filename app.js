a5760d4056ede5f4347b3954903acfa9ae4be499
Johnny Jiang <johnnyj2608@gmail.com>
2024-08-13 23:28:19 -0400

    Fixed menu fetch for new Sheets template

--- app.js
+++ app.js
@@ -44,46 +44,67 @@ app.get("/menu", async (req, res) => {
     const spreadsheetId = "1eNlUPP-Cw50W5PIjTy6HchqL6Yo12KFkAqydLvQsI8M";
 
     try {
-        const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        const today = daysOfWeek[new Date().getDay()];
+        const today = new Date().getDay() - 1;
+        if (today < 0) {
+            return res.status(400).json({ error: 'Invalid day of the week' });
+        }
 
-        const range = "Menu!A1:E15";
+        const range = "Menu!B1:I15";
         const getRows = await googleSheets.spreadsheets.values.get({
             spreadsheetId,
             range,
         });
 
-        const rows = getRows.data.values || [];
-        let breakfastData = [];
-        let lunchData = [];
+        const menuRows = getRows.data.values || [];
+        const breakfastFlag = menuRows[0][4]; 
+        const lunchFlag = menuRows[8][4];
 
-        const breakfastAll = rows[0][4]; 
-        const lunchAll = rows[8][4]; 
+        let breakfastRow = [];
+        let lunchRow = [];
+
+        for (let i = 1; i <= 6; i++) {
+            breakfastRow.push(menuRows[i]);
+        }
+        for (let i = 9; i <= 14; i++) {
+            lunchRow.push(menuRows[i]);
+        }
+
+        if (breakfastFlag === 'TRUE') {
+            breakfastRow = breakfastRow.flat();
+        } else {
+            breakfastRow = breakfastRow[today] || []
+        }
+    
+        if (lunchFlag === 'TRUE') {
+            lunchRow = lunchRow.flat();
+        } else {
+            lunchRow = lunchRow[today] || []
+        }
+
+        let breakfastImg = [];
+        let breakfastTitle = [];
+        let lunchImg = [];
+        let lunchTitle = [];
+
+        for (let i = 0; i < breakfastRow.length; i += 2) {
+            if (breakfastRow[i+1] === "") {
+              break;
+            }
+            breakfastImg.push(breakfastRow[i]);
+            breakfastTitle.push(breakfastRow[i + 1]);
+          }
         
-        for (const row of rows) {
-            if (rows.indexOf(row) >= 1 && rows.indexOf(row) <= 6) {
-                breakfastData.push(row);
+          for (let i = 0; i < lunchRow.length; i += 2) {
+            if (lunchRow[i+1] === "") {
+              break;
             }
-            else if (rows.indexOf(row) >= 9 && rows.indexOf(row) <= 14) {
-                lunchData.push(row);
-            }
-        }
-
-        if (breakfastAll === 'TRUE') { 
-            breakfastData = breakfastData.flat().filter(item => item.trim() !== '');
-        } else {
-            breakfastData = breakfastData.find(row => row[0] === today);
-        }
-
-        if (lunchAll === 'TRUE') {
-            lunchData = lunchData.flat().filter(item => item.trim() !== '');
-        } else {
-            lunchData = lunchData.find(row => row[0] === today);
-        }
+            lunchImg.push(lunchRow[i]);
+            lunchTitle.push(lunchRow[i + 1]);
+          }
 
         const menuData = {
-            breakfast: breakfastData.slice(1),
-            lunch: lunchData.slice(1),
+            breakfast: breakfastTitle,
+            lunch: lunchTitle,
         };
 
         res.render("menu", { menuData });
