a85af7dd035623ded07f43db25077dac59367067
Johnny Jiang <johnnyj2608@gmail.com>
2024-08-21 14:07:17 -0400

    Cache insurance/menu data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const {google} = require("googleapis");
+const NodeCache = require("node-cache");
 
 const app = express();
 
@@ -28,26 +29,35 @@ const insuranceImgMap = {
     "villagecaremax": "/assets/img/insurances/villagecare.png"
 };
 
+const cache = new NodeCache();
+
 // Select insurance and input member ID
 app.get("/", async (req, res) => {
-    const auth = new google.auth.GoogleAuth({
-        keyFile: "credentials.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets",
-    });
+    const cacheKey = "sheetNames";
+    let sheetNames = cache.get(cacheKey);
 
-    const client = await auth.getClient();
-    const googleSheets = google.sheets({version: "v4", auth: client});
-    const spreadsheetId = "1eNlUPP-Cw50W5PIjTy6HchqL6Yo12KFkAqydLvQsI8M";
+    if (!sheetNames) {
+        const auth = new google.auth.GoogleAuth({
+            keyFile: "credentials.json",
+            scopes: "https://www.googleapis.com/auth/spreadsheets",
+        });
 
-    const spreadsheet = await googleSheets.spreadsheets.get({
-        auth,
-        spreadsheetId,
-    });
+        const client = await auth.getClient();
+        const googleSheets = google.sheets({ version: "v4", auth: client });
+        const spreadsheetId = "1eNlUPP-Cw50W5PIjTy6HchqL6Yo12KFkAqydLvQsI8M";
 
-    const excludedSheets = ["breakfast", "lunch", "menu", "qr"];
-    const sheetNames = spreadsheet.data.sheets
-    .map(sheet => sheet.properties.title.toLowerCase())
-    .filter(title => !excludedSheets.includes(title));
+        const spreadsheet = await googleSheets.spreadsheets.get({
+            auth,
+            spreadsheetId,
+        });
+
+        const excludedSheets = ["breakfast", "lunch", "menu", "qr"];
+        sheetNames = spreadsheet.data.sheets
+            .map(sheet => sheet.properties.title.toLowerCase())
+            .filter(title => !excludedSheets.includes(title));
+
+        cache.set(cacheKey, sheetNames, 900);
+    }
 
     res.render("index", { sheetNames, insuranceImgMap });
 });
@@ -55,92 +65,97 @@ app.get("/", async (req, res) => {
 // Fetch daily menu
 app.post("/menu", async (req, res) => {
     const { name, units } = req.body;
+    const cacheKey = "menuItems";
+    let menuData = cache.get(cacheKey);
 
-    const auth = new google.auth.GoogleAuth({
-        keyFile: "credentials.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets",
-    });
-
-    const client = await auth.getClient();
-    const googleSheets = google.sheets({version: "v4", auth: client });
-    const spreadsheetId = "1eNlUPP-Cw50W5PIjTy6HchqL6Yo12KFkAqydLvQsI8M";
-
-    try {
-        const today = new Date().getDay() - 1;
-        if (today < 0) {
-            return res.status(400).json({ error: 'Invalid day of the week' });
-        }
-
-        const range = "Menu!B1:I15";
-        const getRows = await googleSheets.spreadsheets.values.get({
-            spreadsheetId,
-            range,
+    if (!menuData) {
+        const auth = new google.auth.GoogleAuth({
+            keyFile: "credentials.json",
+            scopes: "https://www.googleapis.com/auth/spreadsheets",
         });
 
-        const menuRows = getRows.data.values || [];
-        const breakfastFlag = menuRows[0][4]; 
-        const lunchFlag = menuRows[8][4];
+        const client = await auth.getClient();
+        const googleSheets = google.sheets({ version: "v4", auth: client });
+        const spreadsheetId = "1eNlUPP-Cw50W5PIjTy6HchqL6Yo12KFkAqydLvQsI8M";
 
-        let breakfastRow = [];
-        let lunchRow = [];
-
-        for (let i = 1; i <= 6; i++) {
-            breakfastRow.push(menuRows[i]);
-        }
-        for (let i = 9; i <= 14; i++) {
-            lunchRow.push(menuRows[i]);
-        }
-
-        if (breakfastFlag === 'TRUE') {
-            breakfastRow = breakfastRow.flat();
-        } else {
-            breakfastRow = breakfastRow[today] || []
-        }
-    
-        if (lunchFlag === 'TRUE') {
-            lunchRow = lunchRow.flat();
-        } else {
-            lunchRow = lunchRow[today] || []
-        }
-
-        let breakfastImg = [];
-        let breakfastTitle = [];
-        let lunchImg = [];
-        let lunchTitle = [];
-
-        for (let i = 0; i < breakfastRow.length; i += 2) {
-            if (breakfastRow[i+1] === "") {
-              break;
+        try {
+            const today = new Date().getDay() - 1;
+            if (today < 0) {
+                return res.status(400).json({ error: 'Invalid day of the week' });
             }
-            breakfastImg.push(breakfastRow[i]);
-            breakfastTitle.push(breakfastRow[i + 1]);
-          }
-        
-          for (let i = 0; i < lunchRow.length; i += 2) {
-            if (lunchRow[i+1] === "") {
-              break;
+
+            const range = "Menu!B1:I15";
+            const getRows = await googleSheets.spreadsheets.values.get({
+                spreadsheetId,
+                range,
+            });
+
+            const menuRows = getRows.data.values || [];
+            const breakfastFlag = menuRows[0][4];
+            const lunchFlag = menuRows[8][4];
+
+            let breakfastRow = [];
+            let lunchRow = [];
+
+            for (let i = 1; i <= 6; i++) {
+                breakfastRow.push(menuRows[i]);
             }
-            lunchImg.push(lunchRow[i]);
-            lunchTitle.push(lunchRow[i + 1]);
-          }
-
-        const menuData = {
-            breakfast: {
-                images: breakfastImg,
-                titles: breakfastTitle,
-            },
-            lunch: {
-                images: lunchImg,
-                titles: lunchTitle,
+            for (let i = 9; i <= 14; i++) {
+                lunchRow.push(menuRows[i]);
             }
-        };
 
-        res.render("menu", { name, units, menuData });
+            if (breakfastFlag === 'TRUE') {
+                breakfastRow = breakfastRow.flat();
+            } else {
+                breakfastRow = breakfastRow[today] || []
+            }
 
-    } catch (error) {
-        console.error('Error fetching data from Google Sheets:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+            if (lunchFlag === 'TRUE') {
+                lunchRow = lunchRow.flat();
+            } else {
+                lunchRow = lunchRow[today] || []
+            }
+
+            let breakfastImg = [];
+            let breakfastTitle = [];
+            let lunchImg = [];
+            let lunchTitle = [];
+
+            for (let i = 0; i < breakfastRow.length; i += 2) {
+                if (breakfastRow[i+1] === "") {
+                    break;
+                }
+                breakfastImg.push(breakfastRow[i]);
+                breakfastTitle.push(breakfastRow[i + 1]);
+            }
+
+            for (let i = 0; i < lunchRow.length; i += 2) {
+                if (lunchRow[i+1] === "") {
+                    break;
+                }
+                lunchImg.push(lunchRow[i]);
+                lunchTitle.push(lunchRow[i + 1]);
+            }
+
+            menuData = {
+                breakfast: {
+                    images: breakfastImg,
+                    titles: breakfastTitle,
+                },
+                lunch: {
+                    images: lunchImg,
+                    titles: lunchTitle,
+                }
+            };
+
+            cache.set(cacheKey, menuData, 900);
+
+        } catch (error) {
+            console.error('Error fetching data from Google Sheets:', error);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
     }
+    res.render("menu", { name, units, menuData });
 });
 
 // Confirm member exists
