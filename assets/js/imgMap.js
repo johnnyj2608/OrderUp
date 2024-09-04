5aab81e629774ca5c4c458c2059f9e85f013516d
Johnny Jiang <johnnyj2608@gmail.com>
2024-09-04 12:34:20 -0400

    Translations for insurances

--- assets/js/imgMap.js
+++ assets/js/imgMap.js
@@ -1,21 +1,32 @@
 const insuranceImgMap = {
     "aetna": "/assets/img/insurances/aetna.png",
+    "安泰": "/assets/img/insurances/aetna.png",
+
     "ant": "/assets/img/insurances/anthem.png",
     "abc": "/assets/img/insurances/anthem.png",
     "anthem": "/assets/img/insurances/anthem.png",
     "anthembluecross": "/assets/img/insurances/anthem.png",
+    "蓝十字": "/assets/img/insurances/anthem.png",
+
     "cl": "/assets/img/insurances/centerlight.png",
     "centerlight": "/assets/img/insurances/centerlight.png",
-    "hs": "/assets/img/insurances/hamaspik.png",
-    "hamaspik": "/assets/img/insurances/hamaspik.png",
+    "健信": "/assets/img/insurances/centerlight.png",
+
     "hf": "/assets/img/insurances/homefirst.png",
     "homefirst": "/assets/img/insurances/homefirst.png",
+    "第一家": "/assets/img/insurances/homefirst.png",
+
     "rs": "/assets/img/insurances/riverspring.png",
     "riverspring": "/assets/img/insurances/riverspring.png",
+    "康乐": "/assets/img/insurances/riverspring.png",
+
     "swh": "/assets/img/insurances/seniorwhole.png",
     "seniorwholehealth": "/assets/img/insurances/seniorwhole.png",
+    "长者": "/assets/img/insurances/seniorwhole.png",
+
     "vcm": "/assets/img/insurances/villagecare.png",
-    "villagecaremax": "/assets/img/insurances/villagecare.png"
+    "villagecaremax": "/assets/img/insurances/villagecare.png",
+    "爱心村": "/assets/img/insurances/villagecare.png",
 };
 
 module.exports = insuranceImgMap;
