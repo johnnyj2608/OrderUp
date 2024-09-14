9610e366a2a011bfed761e615dfb70d10a550258
Johnny Jiang <johnnyj2608@gmail.com>
2024-09-14 09:44:19 -0400

    Failed to look up index in views directory

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const i18n = require("i18n");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 const { initializeGoogleSheets } = require('./config/googleAPI');
 
 const indexRoute = require('./routes/indexRoute');
@@ -23,6 +24,7 @@ i18n.configure({
 app.use(cookieParser());
 app.use(i18n.init);
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
